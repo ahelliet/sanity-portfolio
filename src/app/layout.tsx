e05048bd6312e1b7923df9b2e37502d2e10bb2ec
Anthony Helliet <contact@anthonyhelliet.com>
2024-04-18 14:57:00 +0200

    next-themes setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter as FontSans } from "next/font/google"
 import "../styles/globals.css";
 
 import { cn } from "@/lib/utils"
- 
+import { ThemeProvider } from 'next-themes'
+
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
@@ -20,13 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body 
-      className={cn(
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
-        )}
-      >{children}</body>
-    </html>
+        )}>
+        <ThemeProvider attribute='data-theme' enableColorScheme={true} >{children}</ThemeProvider>
+      </body>
+    </html >
   );
 }
